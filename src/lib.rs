5d2ea4c3dcc4629e02ddfffb34c37b12da0fd0d2
Joseph <21144246+joseph-gio@users.noreply.github.com>
2025-04-10 18:59:24 -0700

    unhide unsafe fields

--- src/lib.rs
+++ src/lib.rs
@@ -175,12 +175,12 @@ pub struct SliceRingImpl<T> {
     /// index into `buf` of the first element that could be read.
     /// only gets incremented, never decremented.
     /// wraps around.
-    first_readable: usize,
+    pub first_readable: usize,
     /// index into `buf` where the next element could we written.
     /// only gets incremented, never decremented.
     /// wraps around at `buf.cap()`.
-    next_writable: usize,
-    buf: Vec<T>,
+    pub next_writable: usize,
+    pub buf: Vec<T>,
 }
 
 /// Calculate the number of elements left to be read in the buffer
